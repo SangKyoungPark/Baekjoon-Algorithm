8b1a17b1dbd2e04956374987cf57512173dc1b3b
SangKyoungPark <46203951+SangKyoungPark@users.noreply.github.com>
2020-11-18 23:19:20 +0900

    Update 경사로.cpp

--- 경사로.cpp
+++ 경사로.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 int N, L, ans;
-int a[100][100];
+int map[100][100];
 
 void slope(int i, bool c) {
     int cnt = 1;
     for (int j = 0; j < N - 1; j++) {
-        int d = c == 1 ? a[i][j + 1] - a[i][j] : a[j + 1][i] - a[j][i];
+        int d = c == 1 ? map[i][j + 1] - map[i][j] : map[j + 1][i] - map[j][i];
         if (d == 0) cnt++;
         else if (d == 1 && cnt >= L) cnt = 1;
         else if (d == -1 && cnt >= 0) cnt = -L + 1;
@@ -29,7 +29,7 @@ int main() {
     scanf("%d %d", &N, &L);
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            scanf("%d", &a[i][j]);
+            scanf("%d", &map[i][j]);
         }
     }
     solve();
