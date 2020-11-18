96e74eb3cc1553f010a5b9651efa1ffff0a34b42
SangKyoungPark <46203951+SangKyoungPark@users.noreply.github.com>
2020-11-18 23:18:01 +0900

    Update 경사로.cpp

--- 경사로.cpp
+++ 경사로.cpp
@@ -1,103 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int cal(vector<vector<int>> map, int N, int L) {
+int N, L, ans;
+int a[100][100];
 
-    int cnt = 0;
-    vector<vector<int>> hill(N + 1, vector<int>(N + 1, 0)); //경사로 구간
-
-    for (int i = 1;i <= N;i++) {
-
-        bool check = true;
-
-        //왼쪽에서 오른쪽 검사
-        for (int j = 1;j < N;j++) {
-
-            if (map[i][j] > map[i][j + 1]) {
-
-                //범위 확인
-                if (j + L > N) {
-                    check = false;
-                    break;
-                }
-
-                //높이 차이 1 확인
-                for (int k = j + 1;k <= j + L;k++) {
-                    if (map[i][k] != map[i][j] - 1) {
-                        check = false;
-                        break;
-                    }
-
-                    hill[i][k]++; //높이 차이 1이면 경사로 짓는다
-                }
-
-                if (check == false)
-                    break;
-
-                j += L - 1;
-            }
-
-        }
-
-        //왼쪽부터 검사했을 때 이상있으면 오른쪽부터 검사할 필요 x
-        if (check == false)
-            continue;
-
-        //오른쪽에서 왼쪽으로 검사
-        for (int j = N;j > 1;j--) {
-
-            if (map[i][j] > map[i][j - 1]) {
-
-                if (j - L < 1) {
-                    check = false;
-                    break;
-                }
-
-                for (int k = j - 1;k >= j - L;k--) {
-
-                    //높이 차이 안맞거나 경사로 이미 지은 구간일 때
-                    if (map[i][k] != map[i][j] - 1 || hill[i][k]) {
-                        check = false;
-                        break;
-                    }
-                }
-
-                if (check == false)
-                    break;
-
-                j -= L - 1;
-            }
-        }
-
-        if (check)
-            cnt++;
+void slope(int i, bool c) {
+    int cnt = 1;
+    for (int j = 0; j < N - 1; j++) {
+        int d = c == 1 ? a[i][j + 1] - a[i][j] : a[j + 1][i] - a[j][i];
+        if (d == 0) cnt++;
+        else if (d == 1 && cnt >= L) cnt = 1;
+        else if (d == -1 && cnt >= 0) cnt = -L + 1;
+        else return;
     }
-
-    return cnt;
+    if (cnt >= 0) ans += 1;
 }
+
+void solve() {
+    for (int i = 0; i < N; i++) {
+        slope(i, 1);
+        slope(i, 0);
+    }
+    printf("%d\n", ans);
+}
+
 int main() {
-
-    int N, L;
-    cin >> N >> L;
-
-    int ans = 0;
-    vector<vector<int>> horizon_road(N + 1, vector<int>(N + 1, 0)); //(i,j)
-    vector<vector<int>> vertical_road(N + 1, vector<int>(N + 1, 0)); //(j,i)
-
-    for (int i = 1;i <= N;i++) {
-        for (int j = 1;j <= N;j++) {
-            scanf("%d", &horizon_road[i][j]);
-            vertical_road[j][i] = horizon_road[i][j];
+    scanf("%d %d", &N, &L);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
-
-    ans += cal(horizon_road, N, L);
-    ans += cal(vertical_road, N, L);
-
-    cout << ans << endl;
-
+    solve();
     return 0;
 }
-
