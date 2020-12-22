775a12ae6089ff91889e225686e210022d10ead8
SangKyoungPark <46203951+SangKyoungPark@users.noreply.github.com>
2020-12-22 11:37:09 +0900

    Update 인구이동.cpp

--- 인구이동.cpp
+++ 인구이동.cpp
@@ -1,78 +1,128 @@
 #define _CRT_SECURE_NO_WARNINGS
-
 #include <iostream>
-#include <queue>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 struct point {
-	int y;
-	int x;
+    int y;
+    int x;
 };
-
-queue <point> Q{};
 
 int n{};
 int l{}, r{};
+int nRes{};
+
 int map[64][64]{};
-int res{};
+int temp[64][64]{};
 
-// 차이가 범위에 해당되는지? 해당되면 1 아니면 0
-int isBeApplicable(point p) {
-	int dx[] = { 0,0,1,-1 };
-	int dy[] = { 1,-1,0,0 };
+int dx[] = { -1, 0, 1, 0 };
+int dy[] = { 0, 1, 0, -1 };
 
-	for (int i = 0; i < 4; i++) {
-		int y = p.y + dy[i];
-		int x = p.x + dx[i];
-		if (y < 0 || x < 0 || y >= n || x >= n)	continue;
-		int data = abs(map[y][x] - map[p.y][p.x]);
-		if (l <= data && data <= r)	return 1;
-	}
-	return 0;
+void bfs(point pp) {
+    queue <point> Q{};
+    queue <point> L{};
+
+    int v[64][64]{};
+    int sum = map[pp.y][pp.x];
+
+    v[pp.y][pp.x] = 1;
+
+    Q.push(pp);
+    L.push(pp);
+
+    while (!Q.empty()) {
+        point cur = Q.front();
+        Q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            point p = { cur.y + dy[i], cur.x + dx[i] };
+
+            if (p.y < 0 || p.x < 0 || p.y >= n || p.x >= n) continue;
+            //calculation
+            int data = abs(map[p.y][p.x] - map[cur.y][cur.x]);
+            //printf("data - %d\n", data);
+            if (l <= data && data <= r && !v[p.y][p.x]) {
+                sum += map[p.y][p.x];
+                v[p.y][p.x] = 1;
+                Q.push(p);
+                L.push(p);
+            }
+        }
+    }
+
+    int nData = sum / L.size();
+    while (!L.empty()) {
+        temp[L.front().y][L.front().x] = nData;
+        L.pop();
+    }
 }
 
-void solve() {
-	int sum = 0;
-	//차이가 나는 것 모두 큐에 저장
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			if (isBeApplicable({ i,j})) {
-				Q.push({ i,j });
-				sum += map[i][j];
-			}
-		}
-	}
-	if (sum == 0)	return;
-	int qSize = Q.size();
-	res++;
-	while (!Q.empty()) {
-		point cur = Q.front();
-		Q.pop();
-		map[cur.y][cur.x] = sum / qSize;
-	}
-	solve();
+bool isMigrate(point cur) {
+    for (int i = 0; i < 4; i++) {
+        point p = { cur.y + dy[i], cur.x + dx[i] };
+
+        if (p.y < 0 || p.x < 0 || p.y >= n || p.x >= n) continue;
+        //calculation
+        int data = abs(map[p.y][p.x] - map[cur.y][cur.x]);
+        if (l <= data && data <= r) return true;
+    }
+    return false;
+}
+
+int solve() {
+    int nCnt = 0;
+
+    while (true) {
+        bool flag = false;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                temp[i][j] = map[i][j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isMigrate({ i,j })) {
+                    bfs({ i,j });
+                    flag = true;
+                }
+            }
+        }
+        if (!flag)   break;
+
+        nCnt++;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                map[i][j] = temp[i][j];
+            }
+        }
+    }
+    return nCnt;
 }
 
 int main() {
-	// Input Data
-	scanf("%d", &n);
-	scanf("%d %d", &l, &r);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			scanf("%d", &map[i][j]);
-		}
-	}
-	// Init
-	// Logic
-	solve();
-	// Result
-	printf("%d\n", res);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%d ", map[i][j]);
-		}
-		printf("\n");
-	}
-	return 0;
+    // Input Data
+    scanf("%d %d %d", &n, &l, &r);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &map[i][j]);
+        }
+    }
+    // Logic
+    nRes = solve();
+
+    // Result
+    printf("%d\n", nRes);
+
+    // Test
+    /*
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", map[i][j]);
+        }
+        printf("\n");
+    }
+    */
+    return 0;
 }
