fe4b95cdf46a6b76752ba22a92a0c6bd5b92bfc6
SangKyoungPark <46203951+SangKyoungPark@users.noreply.github.com>
2020-12-25 20:48:14 +0900

    Update 나무재테크.cpp

--- 나무재테크.cpp
+++ 나무재테크.cpp
@@ -1,97 +1,114 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+
+#include <iostream>
 #include <vector>
 #include <algorithm>
-
+#include <cstring>
 using namespace std;
 
-int N{}, M{}, K{};
-int MAT[11][11]{}, A[11][11]{}, D[11][11]{};
+const int SIZE = 16;
 
-vector <int> vec[101]{};
+vector<int> tree[SIZE][SIZE]{};
 
-int dy[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
-int dx[] = { 1, 0, -1, 0, 1, -1, 1, -1 };
+int n, m, k;
+int add[SIZE][SIZE];
+int nut[SIZE][SIZE];
+int map[SIZE][SIZE];
 
-void spring_summer(int loc, int r, int c) {
-	for (int i = 0; i < vec[loc].size(); i++) {
-		int age = vec[loc][i];
-		if (MAT[r][c] >= age) {
-			vec[loc][i] = age + 1;
-			MAT[r][c] -= age;
-		}
-		else {
-			vec[loc].erase(vec[loc].begin() + i);
-			D[r][c] += age / 2;
-			i--;
+int dy[] = { -1,-1,-1,0,0,1,1,1 };
+int dx[] = { -1,0,1,-1,1,-1,0,1 };
+
+void spring_summer() {
+	// 봄 여름
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			sort(tree[i][j].begin(), tree[i][j].end());
+
+			int dNut = 0;
+			vector<int> temp{};
+			for (int k = 0; k < tree[i][j].size(); k++) {
+				if (nut[i][j] >= tree[i][j][k]) {
+					temp.push_back(tree[i][j][k] + 1);
+					nut[i][j] -= tree[i][j][k];
+				}
+				else {
+					dNut += tree[i][j][k] / 2;
+				}
+			}
+			nut[i][j] += dNut;
+			tree[i][j] = temp;
 		}
 	}
 }
 
-void fall(int loc, int r, int c) {
-	int num_tree = vec[loc].size();
-	for (int i = 0; i < num_tree; i++) {
-		int age = vec[loc][i];
-		if (age % 5 != 0) continue;
-
-		for (int n = 0; n < 8; n++) {
-			int nR = r + dy[n]; int nC = c + dx[n];
-			if (nR < 1 || nR > N || nC < 1 || nC > N) continue;
-			int next = (nR - 1) * N + nC;
-			vec[next].push_back(1);
+void fall() {
+	memset(map, 0, sizeof(map));
+	for (int i = 0;i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			for (int k = 0; k < tree[i][j].size(); ++k) {
+				if (tree[i][j][k] % 5 == 0) {
+					for (int next = 0; next < 8; ++next)
+					{
+						int ny = i + dy[next];
+						int nx = j + dx[next];
+						if (!(0 <= ny && ny < n && 0 <= nx && nx < n)) continue;
+						map[ny][nx] += 1;
+					}
+				}
+			}
+		}
+	}
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			for (int k = 0; k < map[i][j]; ++k)
+				tree[i][j].push_back(1);
 		}
 	}
 }
 
 void winter() {
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			MAT[i][j] += D[i][j] + A[i][j];
-			D[i][j] = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			nut[i][j] += add[i][j];
 		}
 	}
 }
 
+
 int main() {
-	//input data
-	scanf("%d %d %d", &N, &M, &K);
-	for (int i = 1; i <= N; i++) {
-		for (int j = 1; j <= N; j++) {
-			scanf("%d", &A[i][j]);
-			MAT[i][j] = 5;
-			vec[(i - 1) * N + j].clear();
+	//Input data
+	scanf("%d %d %d", &n, &m, &k);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			//init nut
+			nut[i][j] = 5;
+			scanf("%d", &add[i][j]);
 		}
 	}
-	for (int i = 1; i <= M; i++) {
-		int x, y, z;
-		scanf("%d %d %d", &x, &y, &z);
-		vec[(x - 1) * N + y].push_back(z);
+	for (int i = 0; i < m; i++) {
+		int y, x, age;
+		scanf("%d %d %d", &y, &x, &age);
+		tree[y - 1][x - 1].push_back(age);
 	}
 
-	//logic
-	for (int k = 1; k <= K; k++) {
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				int loc = (i - 1) * N + j;
-				if (vec[loc].size() == 0) continue;
-				sort(vec[loc].begin(), vec[loc].end());
-				spring_summer(loc, i, j);
-			}
-		}
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				int loc = (i - 1) * N + j;
-				if (vec[loc].size() == 0) continue;
-				fall(loc, i, j);
-			}
-		}
+	//Logic
+	for(int i=0; i<k; i++){
+		spring_summer();
+		//가을
+		fall();
+		//겨울
 		winter();
 	}
-	// result
-	int cnt = 0;
-	for (int i = 1; i <= N * N; i++) {
-		if (vec[i].size() == 0) continue;
-		cnt += vec[i].size();
+
+	//Result
+	int nRes = 0;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j)
+		{
+			nRes += tree[i][j].size();
+		}
 	}
-	printf("%d", cnt);
+	printf("%d\n", nRes);
+
+	return 0;
 }
