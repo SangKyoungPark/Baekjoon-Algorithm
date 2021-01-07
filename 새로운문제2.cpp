196ab63e849282ad35c00b1b603bf4d7fc367bf8
SangKyoungPark <46203951+SangKyoungPark@users.noreply.github.com>
2021-01-08 07:18:23 +0900

    Update 새로운문제2.cpp

--- 새로운문제2.cpp
+++ 새로운문제2.cpp
@@ -2,105 +2,116 @@
 
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-struct INFO {
-	int num{};
-	int y{};
-	int x{};
-	int dir{};
-};
+int N, K;
+int dir[11]; // →:1, ←:2, ↑:3, ↓:4
+int board[12][12]; // 0:흰, 1:빨, 2:파
+int exitTurn = -1; // 말 4개가 쌓이는 종료턴
+bool eixtFlag = false; // 말 4개가 쌓이는 종료조건
 
-int n{}, k{};
-int stage[16][16]{};
-int nRes{};
+int dx[4] = { 0, 0, -1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
 
-vector <INFO> horse{};
-stack <int> map[15][15]{};
+pair<int, int> v[10]; // 말들의 좌표
+vector<int> vInfo[20][20]; // 각각의 셀 안에 쌓이는 말정보 저장
 
-int dy[] = { 0,0,0,-1,1 };
-int dx[] = { 0,1,-1,0,0 };
+bool check_next(int i) {
+	int r = v[i].first;
+	int c = v[i].second;
 
+	if (dir[i] == 1) { if (c + 1 >= N || board[r][c + 1] == 2) return false; }
+	else if (dir[i] == 2) { if (c - 1 < 0 || board[r][c - 1] == 2) return false; }
+	else if (dir[i] == 3) { if (r - 1 < 0 || board[r - 1][c] == 2) return false; }
+	else if (dir[i] == 4) { if (r + 1 >= N || board[r + 1][c] == 2) return false; }
 
-int moving() {
-	for (int i = 1; i <= k; i++) {
-		int y = horse[i].y + dy[horse[i].dir];
-		int x = horse[i].x + dx[horse[i].dir];
+	return true;
+}
 
-		//map out
-		if (y<1 || x<1 || y>n || x>n) {
-			if (horse[i].dir == 1)	horse[i].dir = 2;
-			else if (horse[i].dir == 2)	horse[i].dir = 1;
-			else if (horse[i].dir == 3)	horse[i].dir = 4;
-			else if (horse[i].dir == 4)	horse[i].dir = 3;
-			i--;
-		}
-		//white
-		if (stage[y][x] == 0) {
-			map[horse[i].y][horse[i].x].pop();
-			horse[i].y = y;
-			horse[i].x = x;
-			map[y][x].push(horse[i].num);
-		}
-		//red
-		else if (stage[y][x] == 1) {
-			while (!map[horse[i].y][horse[i].x].empty()) {
-				map[y][x].push(map[horse[i].y][horse[i].x].top());
-				map[horse[i].y][horse[i].x].pop();
-			}
-		}
-		//blue
-		else if (stage[y][x] == 2) {
-			if (horse[i].dir == 1)	horse[i].dir = 2;
-			else if(horse[i].dir == 2)	horse[i].dir = 1;
-			else if (horse[i].dir == 3)	horse[i].dir = 4;
-			else if (horse[i].dir == 4)	horse[i].dir = 3;
-			i--;
-		}
-	}
-
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (map[i][j].size() >= 4)	return 1;
-		}
-	}
+int updateDir(int d) {
+	if (d == 1) return 2;
+	else if (d == 2) return 1;
+	else if (d == 3) return 4;
+	else if (d == 4) return 3;
 	return 0;
 }
 
+void move_piece(int i) {
+	int r = v[i].first;
+	int c = v[i].second;
 
-int main() {
-	int data{};
-	int x{}, y{}, d{};
-	int cnt{};
+	if (dir[i] == 1) { if (c + 1 >= N || board[r][c + 1] == 2) return; }
+	else if (dir[i] == 2) { if (c - 1 < 0 || board[r][c - 1] == 2) return; }
+	else if (dir[i] == 3) { if (r - 1 < 0 || board[r - 1][c] == 2) return; }
+	else if (dir[i] == 4) { if (r + 1 >= N || board[r + 1][c] == 2) return; }
 
-	// Input Data
-	scanf("%d %d", &n, &k);
-	for (int i = 1; i <= n;i++) {
-		for (int j = 1; j <= n;j++) {
-			scanf("%d", &stage[i][j]);
-		}
-	}
-	for (int i = 1; i <= k; i++) {
-		scanf("%d %d %d", &y, &x, &d);
-		horse.push_back({i,y,x,d});
-		map[y][x].push(i);
-	}
-	// Init
-	cnt = 0;
-	nRes = 0;
-
-	// Logic
-	while (true) {
-		if (cnt > 1000) {
-			nRes = -1;
+	int pos = 0;
+	// 1. 해당 셀에서 말이 업힌 스택 위치 찾음
+	for (int p = 0; p < vInfo[r][c].size(); p++) {
+		if (vInfo[r][c].at(p) == i) {
+			pos = p;
 			break;
 		}
-		nRes++;
-		if (moving())	break;
 	}
-	
-	// Result
-	printf("%d\n", nRes);
+	// 2. 업힌 위치 뒤로의 벡터 다음 위치로 옮겨줌
+	int rr = r + dx[dir[i] - 1];
+	int cc = c + dy[dir[i] - 1];
+	vector<int> tmp;
+	for (int p = pos; p < vInfo[r][c].size(); p++) {
+		int pNum = vInfo[r][c].at(p);
+		tmp.push_back(pNum);
+
+		v[pNum].first = rr;
+		v[pNum].second = cc;
+	}
+
+	// 3. 만약 다음 위치가 빨간색이면 말들의 업힌 순서를 뒤집고, 하얀색이면 그대로 둠
+	if (board[rr][cc] == 1) reverse(tmp.begin(), tmp.end());
+	for (int i = 0; i < tmp.size(); i++) {
+		vInfo[rr][cc].push_back(tmp.at(i));
+	}
+
+	if (vInfo[rr][cc].size() >= 4) eixtFlag = true;
+
+	int siz = vInfo[r][c].size();
+	for (int p = pos; p < siz; p++) vInfo[r][c].pop_back();
+}
+
+void solve() {
+	for (int i = 0; i < K; i++) {
+		if (eixtFlag) break;
+		if (!check_next(i)) dir[i] = updateDir(dir[i]);
+		move_piece(i);
+	}
+}
+
+int main() {
+	cin >> N >> K;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> board[i][j];
+		}
+	}
+
+	for (int i = 0; i < K; i++) {
+		cin >> v[i].first >> v[i].second;
+		v[i].first--;
+		v[i].second--;
+		cin >> dir[i];
+		vInfo[v[i].first][v[i].second].push_back(i);
+	}
+
+	for (int i = 0; i < 1000; i++) {
+		solve();
+
+		if (eixtFlag) {
+			exitTurn = i + 1;
+			break;
+		}
+	}
+
+	cout << exitTurn << endl;
 	return 0;
 }
